c290886ec0d5470fd8b674c24791b8d36545c062
FateZeros <490293266@qq.com>
2017-05-17 23:19:26 +0800

    add routes

--- meadowlark.js
+++ meadowlark.js
@@ -18,14 +18,15 @@ var morgan = require('morgan');
 var expLogger = require('express-logger');
 
 var formidable = require('formidable');
+
+var fs = require('fs');
 // 上传
 var jqupload = require('jquery-file-upload-middleware');
-
-var fortune = require('./lib/fortune.js');
 var Weather = require('./lib/weather.js');
 // console.log(weather.getWeatherData());
 
 var app = express();
+// 组织路由
 
 //设置端口
 app.set('port', process.env.PORT || 1212);
@@ -62,15 +63,15 @@ app.use(session());
 // app.set('view engine', '.hbs');
 // set up handlebars view engine
 var handlebars = exphbs.create({
-    defaultLayout:'main',
-    extname: '.hbs',
-    helpers: {
-        section: function(name, options){
-            if(!this._sections) this._sections = {};
-            this._sections[name] = options.fn(this);
-            return null;
-        }
+  defaultLayout:'main',
+  extname: '.hbs',
+  helpers: {
+    section: function(name, options){
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
     }
+  }
 });
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
@@ -85,7 +86,7 @@ app.use(function(req, res, next) {
 	if(!res.locals.partials) res.locals.partials = {};
 	var uid = credentials.weatherApi.uid;
 	var key = credentials.weatherApi.secretKey;
-	var location = 'shenzhen';
+	var location = 'beijing';
 	var argv = require('optimist').default('l', location).argv
 	var weatherApi = new Weather(uid, key);
 	weatherApi.getWeatherNow(argv.l).then(function(data) {
@@ -100,6 +101,23 @@ app.use(function(req, res, next) {
 	next();
 });
 
+require('./routes')(app);
+var autoViews = {}
+app.use(function(req, res, next){
+  var path = req.path.toLowerCase();  
+  // check cache; if it's there, render the view
+  if(autoViews[path]) return res.render(autoViews[path]);
+  // if it's not in the cache, see if there's
+  // a .handlebars file that matches
+  console.log(autoViews, path)
+  if(fs.existsSync(__dirname + '/views' + path + '.hbs')){
+      autoViews[path] = path.replace(/^\//, '');
+      return res.render(autoViews[path]);
+  }
+  // no view found; pass on to 404 handler
+  next();
+});
+
 // jQuery File Upload endpoint middleware
 app.use('/upload', function(req, res, next){
   var now = Date.now();
@@ -112,74 +130,6 @@ app.use('/upload', function(req, res, next){
     }
   })(req, res, next);
 });
-
-/*** 页面路由 Start ***/
-app.get('/', function(req, res) {
-	res.render('home');
-});
-
-app.get('/about', function(req, res) {
-	res.render('about', { 
-		fortune: fortune.getFortune(),
-		pageTestScript: '/qa/tests-about.js' 
-	});
-})
-
-app.get('/jq-test', function(req, res) {
-	res.render('jq-test');
-});
-
-app.get('/tours/hood-river', function(req, res) {
-	res.render('tours/hood-river');
-});
-
-app.get('/tours/request-group-rate', function(req, res) {
-	res.render('tours/request-group-rate');
-});
-
-app.get('/newsletter', function(req, res) {
-	res.render('newsletter', {
-		csrf: 'CSRF token'
-	});
-});
-
-app.post('/process', function(req, res) {
-	console.log('Form (from querystring): ' + req.query.form);
-	console.log('CSRF token (from hidden form field): ' + req.body._csrf); 
-	console.log('Name (from visible form field): ' + req.body.name); 
-	console.log('Email (from visible form field): ' + req.body.email); 
-	if (req.xhr || req.accepts('json,html') === 'json') {
-		res.send({ success: true });
-	} else {
-		res.redirect(303, '/thank-you');
-	}	
-});
-
-app.get('/contest/vacation-photo',function(req,res) { 
-	var now = new Date(); 
-	res.render('contest/vacation-photo', {
-    year: now.getFullYear(),
-    month: now.getMonth()
-  });
-});
-
-app.post('/contest/vacation-photo/:year/:month', function(req, res) { 
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
-		if(err) return res.redirect(303, '/error'); 
-			console.log('received fields:'); 
-			console.log(fields);
-			console.log('received files:'); 
-			console.log(files);
-	    res.redirect(303, '/thank-you');
-    });
-});
-
-app.get('/thank-you', function(req, res) {
-	res.render('thank-you');
-})
-
-/*** 页面路由 END ***/
 
 //定制404页面
 app.use(function(req, res) {
---
+++ routes/index.js
@@ -0,0 +1,74 @@
+// 上传
+var jqupload = require('jquery-file-upload-middleware');
+
+var fortune = require('../lib/fortune.js');
+
+module.exports = function(app) {
+	/*** 页面路由 Start ***/
+	app.get('/', function(req, res) {
+		res.render('home');
+	});
+
+	app.get('/about', function(req, res) {
+		res.render('about', { 
+			fortune: fortune.getFortune(),
+			pageTestScript: '/qa/tests-about.js' 
+		});
+	})
+
+	app.get('/jq-test', function(req, res) {
+		res.render('jq-test');
+	});
+
+	app.get('/tours/hood-river', function(req, res) {
+		res.render('tours/hood-river');
+	});
+
+	app.get('/tours/request-group-rate', function(req, res) {
+		res.render('tours/request-group-rate');
+	});
+
+	app.get('/newsletter', function(req, res) {
+		res.render('newsletter', {
+			csrf: 'CSRF token'
+		});
+	});
+
+	app.post('/process', function(req, res) {
+		console.log('Form (from querystring): ' + req.query.form);
+		console.log('CSRF token (from hidden form field): ' + req.body._csrf); 
+		console.log('Name (from visible form field): ' + req.body.name); 
+		console.log('Email (from visible form field): ' + req.body.email); 
+		if (req.xhr || req.accepts('json,html') === 'json') {
+			res.send({ success: true });
+		} else {
+			res.redirect(303, '/thank-you');
+		}	
+	});
+
+	app.get('/contest/vacation-photo',function(req,res) { 
+		var now = new Date(); 
+		res.render('contest/vacation-photo', {
+	    year: now.getFullYear(),
+	    month: now.getMonth()
+	  });
+	});
+
+	app.post('/contest/vacation-photo/:year/:month', function(req, res) { 
+		var form = new formidable.IncomingForm();
+		form.parse(req, function(err, fields, files) {
+			if(err) return res.redirect(303, '/error'); 
+				console.log('received fields:'); 
+				console.log(fields);
+				console.log('received files:'); 
+				console.log(files);
+		    res.redirect(303, '/thank-you');
+	    });
+	});
+
+	app.get('/thank-you', function(req, res) {
+		res.render('thank-you');
+	});
+
+	/*** 页面路由 END ***/
+}
